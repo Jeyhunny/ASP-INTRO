9ecbd54f6cfa857453d172250cc1d1abf51e4a01
unknown <jeyhunny@code.edu.az>
2023-04-02 21:05:50 +0400

    asp intro

---
+++ ASP-Intro/ASP-Intro-Tasks/ASP-Intro-Tasks/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_Intro_Tasks.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            var users = new List<string>() { "Chinara", "Konul", "Roya", "Elekber", "Ibrahim", "Shaiq" };
+
+            return View(users);
+        }
+    }
+}
---
+++ ASP-Intro/ASP-Intro-Tasks/ASP-Intro-Tasks/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_Intro_Tasks.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //var nums = new List<int>();
+            //for (int i = 1; i <= 20; i++)
+            //{
+            //    if (i % 2 == 1)
+            //    {
+            //        nums.Add(i);
+            //    }
+            //}
+            //return View(nums);
+
+            int[] nums = { };
+            for (int i = 1; i <= 20; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Array.Resize(ref nums, nums.Length + 1);
+                    nums[nums.Length - 1] = i;
+                }
+            }
+            return View(nums);  
+        }
+    }
+}
